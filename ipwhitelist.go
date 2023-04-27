5af579ea870a43ef9224758a6b8b108e72de945a
Andres Felipe Parra <andresparraferreira@MacBook-Pro-de-Andres.local>
2023-04-26 23:31:18 -0500

    init

--- ipwhitelist.go
+++ ipwhitelist.go
@@ -1,62 +1,60 @@
-package ipwhitelist
+package main
 
 import (
 	"context"
 	"net"
 	"net/http"
-	"strings"
-
-	"github.com/traefik/traefik/v2/pkg/config/dynamic"
-	"github.com/traefik/traefik/v2/pkg/ip"
-	"github.com/traefik/traefik/v2/pkg/tracing"
 )
 
-// myIPWhitelist es la estructura que implementa el middleware.
-type myIPWhitelist struct {
-	next           http.Handler
-	whiteLister    *ip.WhiteLister
-	xForwardedFor  bool
-	proxy          bool
-	xRealIPHeader  string
-	name           string
-	tracingHandler *tracing.HTTPHandlerWrapper
+type IPWhitelist struct {
+	allowedIPs []net.IPNet
+	next       http.Handler
 }
 
-// New crea una nueva instancia del middleware myIPWhitelist.
-func New(ctx context.Context, next http.Handler, config *dynamic.MyIPWhitelist, name string) (http.Handler, error) {
-	whiteLister, err := ip.NewWhiteLister(config.SourceRange)
-	if err != nil {
-		return nil, err
+type IPWhitelistConfig struct {
+	AllowedIPs []string `json:"allowedIPs,omitempty"`
+}
+
+func New(ctx context.Context, next http.Handler, config *IPWhitelistConfig, name string) (http.Handler, error) {
+	// Parse the list of allowed IPs from the configuration.
+	allowedIPs := make([]net.IPNet, len(config.AllowedIPs))
+	for i, cidr := range config.AllowedIPs {
+		_, ipNet, err := net.ParseCIDR(cidr)
+		if err != nil {
+			return nil, err
+		}
+		allowedIPs[i] = *ipNet
 	}
 
-	return &myIPWhitelist{
-		next:          next,
-		whiteLister:   whiteLister,
-		xForwardedFor: config.XForwardedFor,
-		proxy:         config.Proxy,
-		xRealIPHeader: config.XRealIPHeader,
-		name:          name,
+	return &IPWhitelist{
+		allowedIPs: allowedIPs,
+		next:       next,
 	}, nil
 }
 
-// ServeHTTP se encarga de interceptar las solicitudes y verificar si la dirección IP está en la lista blanca.
-func (m *myIPWhitelist) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	ipAddress := req.RemoteAddr
-	if m.xForwardedFor {
-		ipAddress = req.Header.Get("X-Forwarded-For")
-	}
-
-	if m.proxy && m.xRealIPHeader != "" {
-		ipAddress = req.Header.Get(m.xRealIPHeader)
-	}
-
-	ipAddress = strings.Split(ipAddress, ":")[0]
-
-	isAllowed := m.whiteLister.IsAllowed(net.ParseIP(ipAddress))
-	if !isAllowed {
-		rw.WriteHeader(http.StatusForbidden)
+func (p *IPWhitelist) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	// Get the IP address of the request.
+	ip, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
-	m.next.ServeHTTP(rw, req)
+	// Check if the IP address is allowed.
+	allowed := false
+	for _, ipNet := range p.allowedIPs {
+		if ipNet.Contains(net.ParseIP(ip)) {
+			allowed = true
+			break
+		}
+	}
+
+	// If the IP address is not allowed, return a 403 Forbidden status.
+	if !allowed {
+		http.Error(rw, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		return
+	}
+
+	// If the IP address is allowed, pass the request to the next handler.
+	p.next.ServeHTTP(rw, req)
 }
